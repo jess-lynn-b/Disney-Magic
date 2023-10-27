907fb5de6aa7a06c3f36323cdd49522b70d94d9c
Jessica Blackwell <138169206+jess-lynn-b@users.noreply.github.com>
2023-10-27 17:15:40 -0500

    new changes to project

--- src/app/palo/palo.service.ts
+++ src/app/palo/palo.service.ts
@@ -10,7 +10,7 @@ PaloDiningService {
   private myPaloDineArray:
   PaloDine[] = [
     new PaloDine
-    ('Palo', 'Savor classic Northern Italian cooking with a modern twist at this chic adult-exclusive restaurant—it’s a feast for the senses!', 'http'),
+    ('Palo', 'Savor classic Northern Italian cooking with a modern twist at this chic adult-exclusive restaurant—it’s a feast for the senses!', 'https://www.mickeysdreamvacations.com/uploads/1/2/1/4/12146140/jen6_orig.jpg'),
   ]
   getData(){
     return this.myPaloDineArray;
